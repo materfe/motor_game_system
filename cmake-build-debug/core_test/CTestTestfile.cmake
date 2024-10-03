227138d5f7bc9a50be11cbdf87063d7c880e07f8
materfe <mathieurouxdasilva2005@gmail.com>
2024-10-03 15:02:45 +0200

    destop

---
+++ cmake-build-debug/core_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Mat/special_repo/game_motor_system/motor_game_system/core_test
+# Build directory: C:/Users/Mat/special_repo/game_motor_system/motor_game_system/cmake-build-debug/core_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
