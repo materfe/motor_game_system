d740d28039938c381078bfacd1bb5d38063376eb
materfe <mathieurouxdasilva2005@gmail.com>
2024-10-29 16:57:40 +0100

    alllocaotr

---
+++ api/include/custom_allocator.h
@@ -0,0 +1,23 @@
+﻿//
+// Created by Mat on 29.10.2024.
+//
+
+#ifndef GAME_MOTOR_ENGINE_API_INCLUDE_CUSTOM_ALLOCATOR_H_
+#define GAME_MOTOR_ENGINE_API_INCLUDE_CUSTOM_ALLOCATOR_H_
+
+template<typename T>
+class StandardAllocator
+{
+ public:
+  using value_type = T;
+  T* allocate(size_t n)
+  {
+    return ::operator new(n);
+  }
+  void deallocate(T* ptr, [[maybe_unused]] size_t n)
+  {
+    return ::operator delete(ptr);
+  }
+};
+
+#endif //GAME_MOTOR_ENGINE_API_INCLUDE_CUSTOM_ALLOCATOR_H_
